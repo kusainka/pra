845cd11af5d76c8405e253ed54078dc25bc78df7
kusainka <57935545+kusainka@users.noreply.github.com>
2019-11-21 01:38:16 +0500

    Create 37.cpp

---
+++ 37.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{  int a, b;
+   cout << "Please enter two digits: ";
+    cin >>a>>b;
+   if (a > b){
+    cout << a<<endl;}
+        
+    else if (b > a)
+    {
+        cout << b;}}
