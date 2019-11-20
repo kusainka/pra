f994d34a1c2e01ec2594d97d848c4c0631f3dcc5
kusainka <57935545+kusainka@users.noreply.github.com>
2019-11-21 01:22:57 +0500

    Create 51.cpp

---
+++ 51.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int x,y,z;
+    cin>>x>>y>>z;
+    if (x+y==z || x+z==y || y+z==x)
+        cout << "yes";
+    else cout<< "no";
+    return 0;
+}
