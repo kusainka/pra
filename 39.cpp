cc15002b6c5cab1a7c0a216c31901f98542e6467
kusainka <57935545+kusainka@users.noreply.github.com>
2019-11-21 01:54:16 +0500

    Create 39.cpp

---
+++ 39.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{  int a, b;
+   cout << "Please enter two digits: ";
+    cin >>a>>b;
+   if (a>b){
+    cout << "Yes"<<endl;}
+        
+    else 
+    { int d=b;
+      int c=a;
+        cout <<d<<" "<<c;}}
