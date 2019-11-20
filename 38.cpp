7de69942dd001032ea0116c37c101750f6d493d9
kusainka <57935545+kusainka@users.noreply.github.com>
2019-11-21 02:00:56 +0500

    Create 38.cpp

---
+++ 38.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{  int a, b;
+   cout << "Please enter two digits: ";
+    cin >>a>>b;
+   if ((a-b)>=100||(b-a)>=100){
+    cout << "Yes\n";}
+        
+    else 
+    {
+        cout <<"No\n";}}
