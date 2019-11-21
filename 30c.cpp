4c2e58496834c21bf3c46b30ff6743738afd3e48
Zhadyra Bekmagambet IT1906 <57935545+kusainka@users.noreply.github.com>
2019-11-21 10:56:34 +0600

    Create 30c.cpp

---
+++ 30c.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a,b,c,d,e; 
+    cin>>a;
+    b=a*a; 
+    c=b*a; 
+    d=c*c; 
+    e=d*d; 
+    a=c*e; 
+    cout<<a<<endl;
+}
