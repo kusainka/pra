405d21357ade7321feafd2e6c9497ba1959031ff
Zhadyra Bekmagambet IT1906 <57935545+kusainka@users.noreply.github.com>
2019-11-21 11:28:41 +0600

    Create 99.cpp

---
+++ 99.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c,d;
+    for (int i=1000;i<=9999;i++){
+    a=(i/1000);
+    b=(i/100)%10;
+    c=(i/10)%10;
+    d=(i%10);
+    if ((a!=b)&&(a!=c)&&(a!=d)&&(b!=c)&&(b!=d)&&(c!=d))
+        cout<<i<<endl;
+    }
+}
