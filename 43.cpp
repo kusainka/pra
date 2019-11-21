ff518c894ec19d2dd2ef499d48fba0c0cdfc1d1d
Zhadyra Bekmagambet IT1906 <57935545+kusainka@users.noreply.github.com>
2019-11-21 11:01:36 +0600

    Create 43.cpp

---
+++ 43.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int a;
+cin>>a;
+if (((a>=1) && (a<=2))||(a==12)){
+    cout<<"Winter";
+} else if ((a>=3) && (a<=5)){
+    cout<<"Spring";
+} else if ((a>=6) && (a<=8)){
+    cout<<"Summer";
+} else if ((a>=9) && (a<=11)){
+    cout<<"Autumn";
+}
+}
