6da973f1a916511e928b2b49e87db4fe97143927
Zhadyra Bekmagambet IT1906 <57935545+kusainka@users.noreply.github.com>
2019-11-21 10:57:25 +0600

    Create 32.cpp

---
+++ 32.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int found,find,x1,x2,x3;
+found=237;
+x3=found/100;
+x1=found/10%10;
+x2=found%10;
+find=x1*100+x2*10+x3;
+cout<<find;
+}
