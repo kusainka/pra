65bb198a9820cea5a2b469a5655a62bc1a5b9aab
Zhadyra Bekmagambet IT1906 <57935545+kusainka@users.noreply.github.com>
2019-11-21 11:26:34 +0600

    Create 92.cpp

---
+++ 92.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double answer=0,i,n;
+    n=100;
+    for (i=1;i<=n;i++){
+        answer+=i;
+    }
+    cout<<answer;
+    return 0;
+}
