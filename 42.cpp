22da5d05894d818f14eb0a305430cfc3f07ceefd
Zhadyra Bekmagambet IT1906 <57935545+kusainka@users.noreply.github.com>
2019-11-21 11:01:05 +0600

    Create 42.cpp

---
+++ 42.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int a;
+cin>>a;
+if ((a>=2)&&(a<=5)){
+    a+=10;
+} else if ((a>7)&&(a<40)){
+    a-=100;
+} else if ((a<=0)||(a>3000)){
+    a*=3;
+} else {
+    a=0;
+}
+cout<<a;
+}
