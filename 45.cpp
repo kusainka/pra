faf3f78ce77737c3db9211d812f793a46f1caba0
Zhadyra Bekmagambet IT1906 <57935545+kusainka@users.noreply.github.com>
2019-11-21 11:02:23 +0600

    Create 45.cpp

---
+++ 45.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int a,b,c;
+cin>>a>>b>>c;
+if ((a>10)&&(b>10)&&(c>10)&&(a%3==0)&&(b%3==0))
+    cout<<"yes";
+else 
+    cout<<"no";
+}
