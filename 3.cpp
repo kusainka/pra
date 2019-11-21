2c421aa87e1efa89ea4bedc0e7f4814d6bf183ce
Zhadyra Bekmagambet IT1906 <57935545+kusainka@users.noreply.github.com>
2019-11-21 10:36:41 +0600

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    for (int i=1;i<6;i++){
+        for (int j=1;j<i+1;j++){
+            cout<<0;
+        }
+        cout<<endl;
+    }
+}
