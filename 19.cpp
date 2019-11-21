12a747f2747f91ca8dcd5d46b014d54f7ea9b7e4
Zhadyra Bekmagambet IT1906 <57935545+kusainka@users.noreply.github.com>
2019-11-21 10:49:46 +0600

    Create 19.cpp

---
+++ 19.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    double a,b;
+    cin>>a>>b;
+    cout<<"a) "<<((3*a)/10)+((4*b)/10)<<endl;
+    cout<<"b) "<<3*(((2000*a)/1000)+((1800*b)/1000))<<endl;
+}
