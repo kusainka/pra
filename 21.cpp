d93b7da11cdedf0aee26124fa62abb12480f55ca
Zhadyra Bekmagambet IT1906 <57935545+kusainka@users.noreply.github.com>
2019-11-21 10:50:57 +0600

    Create 21.cpp

---
+++ 21.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    double a,b,c;
+    cin>>a>>b;
+    c=sqrt(a*a+b*b);
+    cout<<(a*b)/2<<endl;
+    cout<<a+b+c<<endl;
+    cout<<c;
+}
