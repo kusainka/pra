ba57d5567d5902a0dc2c80f6b9c33d25c2aafaa4
Zhadyra Bekmagambet IT1906 <57935545+kusainka@users.noreply.github.com>
2019-11-21 11:24:01 +0600

    Create 86.cpp

---
+++ 86.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,answer=0;
+    cin>>n;
+    for (int i=1;i<=n;i++){
+        answer+=i;
+    }
+    cout<<answer;
+    return 0;
+}
